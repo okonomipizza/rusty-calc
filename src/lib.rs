4e1b6ee5e43059368c0edbc915f27da6e9441f0c
okonomipizza <140386510+okonomipizza@users.noreply.github.com>
2024-10-14 17:10:19 +0900

    字句解析機能を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
+pub mod calc;
+pub mod lexer;
 use core::str;
-use std::io::{self, Error, Stdin, Write};
+use std::io::{self, Error, Write};
 
-// 最後の文字が.になるまで入力を受け続ける
-// 入力はvec[]として保持していく。
-// 得られたvec[]は行ごとになるので有効なtokenに変換する
-pub fn get_user_input() -> Result<Vec<String>, Error> {
-    // 行番号を表示
-    print!(">");
-    io::stdout().flush().unwrap();
-
+/// ユーザーからの入力を受け付ける
+pub fn get_user_input(current_row: &mut i32) -> Result<Vec<String>, Error> {
     let mut inputs: Vec<String> = Vec::new();
 
-    // . + Enter が入力されるまで入力を受け付ける
+    // ;; + Enter が入力されるまで入力を受け付ける
     let mut input_continue = true;
     while input_continue {
+        print!("#{}  ", current_row);
+        io::stdout().flush().unwrap();
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
@@ -22,61 +20,73 @@ pub fn get_user_input() -> Result<Vec<String>, Error> {
                 }
 
                 inputs.push(input);
+                *current_row += 1;
             }
             Err(err) => {
-                eprintln!("Error: {}", err)
+                eprintln!("Error: {}", err);
+                return Err(err);
             }
         }
     }
-
     Ok(inputs)
 }
 
-// 与えられた行の最後の文字が.かを判断
+/// get_user_input() のヘルパー関数
+/// 与えられた行が ;; で終わるか否かを判断する
 fn is_final_input(input: &str) -> bool {
     let trimmed_input = input.trim();
     if trimmed_input.is_empty() {
         return false;
     }
-    trimmed_input.ends_with('.')
+    trimmed_input.ends_with(";;")
 }
 
 #[test]
-fn test_is_final_input_ends_with_period() {
-    assert_eq!(is_final_input("Hello world."), true);
+fn test_is_final_input_ends_with_semi_colons() {
+    assert!(is_final_input("Hello world;;"));
+}
+
+#[test]
+fn test_is_final_input_ends_with_a_semi_colons() {
+    assert!(!is_final_input("Hello world;"))
+}
+
+#[test]
+fn test_is_final_input_ends_with_semi_colons_blank_semi_colons() {
+    assert!(!is_final_input("Hello world; ;"));
 }
 
 #[test]
 fn test_is_final_input_with_trailing_whitespace() {
-    assert_eq!(is_final_input("Hello world.  "), true);
+    assert!(is_final_input("Hello world;;  "));
 }
 
 #[test]
-fn test_is_final_input_no_period() {
-    assert_eq!(is_final_input("Hello world"), false);
+fn test_is_final_input_no_semi_colons() {
+    assert!(!is_final_input("Hello world"));
 }
 
 #[test]
 fn test_is_final_input_empty_string() {
-    assert_eq!(is_final_input(""), false)
+    assert!(!is_final_input(""))
 }
 
 #[test]
 fn test_is_final_input_with_only_whitespace() {
-    assert_eq!(is_final_input("   "), false)
+    assert!(!is_final_input("   "))
 }
 
 #[test]
 fn test_is_final_input_with_newline() {
-    assert_eq!(is_final_input("Hello world.\n"), true);
+    assert!(is_final_input("Hello world;;\n"));
 }
 
 #[test]
 fn test_is_final_input_with_multiple_lines_no_period() {
-    assert_eq!(is_final_input("Hello\nworld"), false)
+    assert!(!is_final_input("Hello\nworld"))
 }
 
 #[test]
 fn test_is_final_input_with_multiple_lines_ends_with_period() {
-    assert_eq!(is_final_input("Hello\nworld."), true)
+    assert!(is_final_input("Hello\nworld;;"))
 }
